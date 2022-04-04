70cdf3285268754e369b66a4d5503684201a909f
BlueRoseStory <scottb@tsbrooks.com>
2022-04-03 22:29:09 -0400

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   user1 = 'John Doe';
   user2 = 'Jane Doe';
-  maxCharText = 'This text is being passed to the max-characters web component';
+  maxCharText = 'Type here...';
   token =
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c';
 }
